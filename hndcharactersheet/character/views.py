d4369cc373c5bb005392242a3e53690f80503b20
Nathan Romano <nathan.romano@meria.com>
2024-07-31 17:06:02 +0200

    feat: add basic html css file

--- hndcharactersheet/character/views.py
+++ hndcharactersheet/character/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def fiche_personnage(request):
+    return render(request, 'characters/fiche.html')
--- hndcharactersheet/hndcharactersheet/urls.py
+++ hndcharactersheet/hndcharactersheet/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('fiche/', views.fiche_personnage, name='fiche_personnage'),
 ]
